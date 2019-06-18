47f2958443907c6a365961b4743bc87565451a9a
June Hyok Cho <jcho0627@gmail.com>
2019-06-18 12:53:50 +0200

    adding middleware to update and delete route

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -64,7 +64,7 @@ router.get("/:id/edit", checkCampgroundOwnership, (req, res) => {
 });
 
 // UPDATE campground route
-router.put("/:id", (req, res) => {
+router.put("/:id", checkCampgroundOwnership, (req, res) => {
   // Find and update the correct campground
   Campground.findByIdAndUpdate(req.params.id, req.body.campground, (err, updatedCampground) => {
     if (err) {
@@ -77,7 +77,7 @@ router.put("/:id", (req, res) => {
 });
 
 // DESTROY campground route
-router.delete("/:id", (req, res) => {
+router.delete("/:id", checkCampgroundOwnership, (req, res) => {
   Campground.findByIdAndRemove(req.params.id, (err) => {
     if (err) {
       res.redirect("/campgrounds");
